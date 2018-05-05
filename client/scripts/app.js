6dcb75d493497a4df26e586bb61e50e93023cda5
fyun89 <fyun89@gmail.com>
2018-05-04 21:31:23 -0700

    Refresh button work in progress.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,28 @@
 // YOUR CODE HERE:
 let app = {};
 
+app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+
 app.init = function() {
   $('#chats').on('click', '.username', function() {
     app.handleUsernameClick();
   });
   $('#send .submit').submit(app.handleSubmit);
+  $('#main').on('click', '.refresh', function() {
+    app.clearMessages();
+    let messages = app.fetch().results;
+    for (var i = 0; i < messages.length; i++) { //currently not working (Cannot read property 'results')
+      if (messages[i].username !== undefined) {
+        app.renderMessage(messages[i]);
+      }
+    }
+  });
 };
 
 app.send = function(input) {
   $.ajax({
     type: 'POST',
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     data: JSON.stringify(input),
     success: console.log('yay!!'),
     contentType: 'application/json'
@@ -21,8 +32,13 @@ app.send = function(input) {
 app.fetch = function() {
   $.ajax({
     type: 'GET',
+    url: app.server,
+    data: {
+      limit: 30,
+      order: '-createdAt'
+    },
+    contentType: 'application/json',
     success: function(data) {
-      JSON.stringify(data.message);
     }
   });
 };
@@ -32,7 +48,7 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  $('#chats').append('<p>' + '<span class=\'username\'>' + message.username + '</span>: ' + message.text + '</p>');
+  $('#chats').prepend('<p>' + '<span class=\'username\'>' + message.username + '</span>: ' + message.text + '</p>');
 };
 
 app.renderRoom = function(room) {
@@ -46,3 +62,8 @@ app.handleUsernameClick = function() {
 app.handleSubmit = function() {
   
 };
+
+$(document).ready(function() {
+  app.init();
+  $('h2').append(window.location.search.slice(10));
+});
